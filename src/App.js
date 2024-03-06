513e707b756304ebe8d6bdc8fb1055934136db1c
ReziMi <revaz.mikadze92@gmail.com>
2024-03-07 03:27:55 +0400

    add some styling and bg photo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
+  // const url=`https://api.openweathermap.org/data/2.5/forecast?q=london&appid=8d014cf53186113d79860b03d2a3f913`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>London</p>
+          </div>
+          <div className="temp">
+            <h1>65F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        
+        <div className="bottom">
+          <div className="feels">
+            <p>60F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12mph</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
